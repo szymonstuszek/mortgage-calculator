f6b9698aefb099537e081b7a3fb3785d9a3e4620
szymon <szymonstuszek@gmail.com>
2023-08-06 20:56:59 +0200

    fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [
+      RouterTestingModule, 
+      MatFormFieldModule, 
+      MatInputModule, 
+      FormsModule, 
+      MatButtonModule, 
+      MatIconModule, 
+      MatCardModule,
+      BrowserAnimationsModule
+    ],
     declarations: [AppComponent]
   }));
 
@@ -24,6 +40,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mortgage-calculator app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain('mortgage-calculator app is running!');
   });
 });
