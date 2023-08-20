4fd9d97a962d8b3ac191f44bdeeea4c5927ca2d6
szymon <szymonstuszek@gmail.com>
2023-08-20 11:56:47 +0200

    event type

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
